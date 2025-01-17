c0d515f31fdf78ce646fb1619b06054e2280a15b
zennon-sml <zennonsampaioml@gmail.com>
2025-01-17 18:11:03 -0300

    feat: labs report

--- controllers/labController.js
+++ controllers/labController.js
@@ -1,6 +1,8 @@
 const multer = require('multer');
 const path = require('path');
 const Lab = require('../models/lab');
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
 
 // Configurar armazenamento para uploads
 const storage = multer.diskStorage({
@@ -33,3 +35,58 @@ exports.createLab = async (req, res) => {
     res.status(500).json({ message: 'Erro ao criar laboratório', error });
   }
 };
+
+exports.generateReport = async (req, res) => {
+    try {
+        const labs = await Lab.find()
+
+        const doc = new PDFDocument()
+        const filePath = path.join(__dirname, '..', 'uploads', 'relatorio.pdf')
+        
+        const writeStream = fs.createWriteStream(filePath)
+        doc.pipe(writeStream)
+        
+        doc.fontSize(20).text('Relatorios dos Laboratórios', { align: 'center' });
+        doc.moveDown();
+    
+        // Adicionar cada laboratório no relatório
+        labs.forEach((lab) => {
+          doc.fontSize(14).text(`Nome: ${lab.name}`);
+          doc.text(`Descrição: ${lab.description}`);
+          doc.text(`Capacidade: ${lab.capacity}`);
+          if (lab.picture) {
+            const imagePath = path.join(__dirname, '..', lab.picture);
+            if (fs.existsSync(imagePath)) {
+              doc.image(imagePath, { width: 150, height: 100 });
+            }
+          }
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+          doc.moveDown();
+        });
+    
+        // Finalizar o documento
+        doc.end();
+
+        writeStream.on('finish', () => {
+            res.download(filePath, 'relatorio_laboratorios.pdf', (err) => {
+              if (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Erro ao baixar arquivo.' });
+              } else {
+                // Remover o arquivo temporário após o download
+                fs.unlinkSync(filePath);
+              }
+            });
+          });
+    } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Erro ao gerar arquivo.', error });
+    }
+}
--- routes/labRoutes.js
+++ routes/labRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { createLab, uploadMiddleware} = require('../controllers/labController')
+const { createLab, uploadMiddleware, generateReport} = require('../controllers/labController')
 const router = express.Router()
 
 router.post('/lab/new', uploadMiddleware, createLab)
+router.get('/laboratorios/relatorio', generateReport)
 
 module.exports = router
