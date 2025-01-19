ca9a437221e2a6779df19ad8bec7071731f4e220
zennon-sml <zennonsampaioml@gmail.com>
2025-01-19 07:16:25 -0300

    fix: vercel.json route dest

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.get('/', (req, res) => {
 app.use('/api', authRoutes)
 app.use('/api', labRoutes)
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => {
-    console.log(`Server running on localhost:${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
