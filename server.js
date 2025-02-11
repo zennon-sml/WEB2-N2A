198ca41b26e4d4c5dc20bd8014df698e42e8145d
zennon-sml <zennonsampaioml@gmail.com>
2025-02-11 17:25:18 -0300

    feat: block lab and emit new temp

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const labRoutes = require('./routes/labRoutes')
 const videoRoutes = require('./routes/videoRoute')
 const weekdayMiddleware = require('./middlewares/weekdayMiddleware')
 const path = require("path")
+const http = require('http');
+const { Server } = require('socket.io');
+
 
 dotenv.config()
 
 const app = express()
+// Socket
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+      origin: "*",  // Permite conexões de qualquer origem (ajuste conforme necessário)
+      methods: ["GET", "POST"]
+  }
+});
 
 app.use(weekdayMiddleware)
 
@@ -21,21 +32,41 @@ mongoose.connect(process.env.MONGO_URI)
 });
 
 app.use(express.json())
-
-app.use('/api', authRoutes)
-app.use('/api', labRoutes)
-app.use('/api', videoRoutes)
+app.use(authRoutes)
+app.use(labRoutes)
+app.use(videoRoutes)
 app.use(express.static(path.join(__dirname, "public")));
 
 
+// Socket
+// Bloquear Lab
+app.post('/bloquear/:lab', (req, res) => {
+  const lab = req.params.lab;
+  io.emit('bloquearLab', lab);
+  res.send(`Laboratório ${lab} bloqueado!`);
+});
+// Temperaturas
+app.post('/temperatura/:temp', (req, res) => {
+  const { temp } = req.params;
+  io.emit('atualizarTemperatura', temp ); 
+  res.send(`Temperatura do laboratório atualizada para ${temp}°C`);
+});
+
+io.on('connection', (socket) => {
+  console.log('Novo usuário conectado');
+  socket.on('disconnect', () => {
+      console.log('Usuário desconectado');
+  });
+});
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 //DONE A.    Ter uma rota GET para '/videoTutorial', que envia, usando stream um vídeo sobre como usar a API, o vídeo deve ter pelo menos 1 minuto de duração.
-//B.    Ter uma rota POST para ‘/bloquear/:lab’, que envia um emit para o canal ‘bloquear(lab)’ para todos os clientes ouvintes desse canal, criar um frontend HTML + Javascript simples, apenas para mostrar na tela quando alguém bloquear um laboratório, todos os clientes desse HTML devem ver a mensagem que algum laboratório foi bloqueado.
+//DONE B.    Ter uma rota POST para ‘/bloquear/:lab’, que envia um emit para o canal ‘bloquear(lab)’ para todos os clientes ouvintes desse canal, criar um frontend HTML + Javascript simples, apenas para mostrar na tela quando alguém bloquear um laboratório, todos os clientes desse HTML devem ver a mensagem que algum laboratório foi bloqueado.
 //C.   Usando sensor de temperatura, monitore a temperatura de 1 laboratório em tempo real, para isso, use um simulador de hardware
 //D.   Ter uma rota ‘/temperaturaAtual’, que exibe a temperatura do laboratório naquele momento
 //E.    Ter uma rota ‘/ligarLuz’, que liga a luz de 1 laboratório, para isso, utilize um simulador de hardware
