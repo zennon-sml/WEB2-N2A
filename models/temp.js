c2b04fe5b5746dcd41e7dbe2a51a55ab163dfc21
zennon-sml <zennonsampaioml@gmail.com>
2025-02-11 21:05:21 -0300

    fix: timestamps for finding latest

--- models/temp.js
+++ models/temp.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-const TempSchema = new mongoose.Schema({
-    temp: { type: String, required: true },
-})
+const TempSchema = new mongoose.Schema(
+    { temp: { type: String, required: true }},
+    { timestamps: true },
+)
 
 const Temp = mongoose.model('Temp', TempSchema)
 module.exports = Temp
