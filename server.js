2eba57ec2080d6d1d26dbd55531b9649861bab2f
zennon-sml <zennonsampaioml@gmail.com>
2025-02-16 19:35:37 -0300

    vide path

--- server.js
+++ server.js
@@ -72,3 +72,8 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+//A. Ter uma rota GET para '/videoTutorial', que envia, usando stream um vídeo sobre como usar a API, o vídeo deve ter pelo menos 1 minuto de duração.
+//B. Ter uma rota POST para ‘/bloquear/:lab’, que envia um emit para o canal ‘bloquear(lab)’ para todos os clientes ouvintes desse canal, criar um frontend HTML + Javascript simples, apenas para mostrar na tela quando alguém bloquear um laboratório, todos os clientes desse HTML devem ver a mensagem que algum laboratório foi bloqueado.
+//C. Usando sensor de temperatura, monitore a temperatura de 1 laboratório em tempo real, para isso, use um simulador de hardware
+//D. Ter uma rota ‘/temperaturaAtual’, que exibe a temperatura do laboratório naquele momento
+//E. Ter uma rota ‘/ligarLuz’, que liga a luz de 1 laboratório, para isso, utilize um simulador de hardware
